bd58e77db27572b0f7e8b4a872dd5ea431a210b7
jianzuming <jianzuming@taihe.com>
2019-12-23 10:04:11 +0800

    update

---
+++ 1000.合并石头的最低成本.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=1000 lang=cpp
+ *
+ * [1000] 合并石头的最低成本
+ */
+
+// @lc code=start
+
+#include <vector>
+
+class Solution {
+public:
+    int mergeStones(vector<int>& stones, int K) {
+        
+    }
+};
+// @lc code=end
+
